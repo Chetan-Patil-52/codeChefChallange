cc398f5496a2901f7a0a38b70cfeabe9d44e9ec2
Chetan D Patil <121055212+Chetan-Patil-52@users.noreply.github.com>
2025-02-05 21:29:10 +0530

    Create code.cpp

---
+++ Contests/ Starters 172/Time Machine/code.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x;
+    cin>>x;
+    if(x>=2025 && x<=2050){
+        cout<<"Yes\n";
+    }else{
+        cout<<"No\n";
+    }
+}
